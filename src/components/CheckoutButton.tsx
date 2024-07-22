dc1062795e5fa6d2c2634dea83368b2b79c5aa80
Raphael Verdier <verdier.raphael@gmail.com>
2024-07-22 11:38:12 +0200

    First commit

---
+++ src/components/CheckoutButton.tsx
@@ -0,0 +1,7 @@
+export const CheckoutButton = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  )
+}
---
+++ src/stories/Test.stories.ts
@@ -0,0 +1,10 @@
+import { CheckoutButton } from "../components/CheckoutButton"
+
+export default {
+  title: "Example/CheckoutButton",
+  component: CheckoutButton
+}
+
+export const CheckoutButtonDefault = {
+  args: {}
+}
